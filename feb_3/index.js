ade5bc5f8c0c80d69d4239dc29447341553f6eb7
Sean Golob <seangolob@live.com>
2015-02-03 09:57:55 -0800

    add extra credit command line arguments

--- feb_3/index.js
+++ feb_3/index.js
@@ -3,3 +3,11 @@
 var hello = require('./lib/greeting');
 
 console.log(hello.greet('Sean'));
+
+// Command line utility that uses proccess.argv
+var myArg = process.argv[2];
+
+if(myArg) {
+  console.log(hello.greet(myArg));
+};
+
